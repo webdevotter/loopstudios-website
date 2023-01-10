0ce5b575ca4dd1a2765ab7765ea3e5c469f297b8
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2023-01-10 14:06:00 -0600

    Creations Section With Gradient Overlays

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         sans: ['Josefin', 'sans-serif'],
         alata: ['Alata'],
       },
+      letterSpacing: {
+        widest: '.3em',
+      }
     },
   },
   plugins: [],
